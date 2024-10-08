38a095eec9470ea50a06b5d31b72a6626a1fac7a
Kresna Satya <kresnasatya72@gmail.com>
2024-10-08 21:19:36 +0800

    feat: create books-grid class

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <h1>buku.kresna.me</h1>
 <p>These are my books collection. Half of them I have read and others not yet. How about you?</p>
 
-<div style="display: grid; grid-template-columns: repeat(3, 1fr); gap: 1rem;">
+<div class="books-grid">
     {#each data.books as book}
         <div class="p-4" style="display: flex; flex-direction: column; background-color: lightgray; align-items: center;">
             <h2 style="text-align: center;"><a href={`books/${book.slug}`}>{book.title}</a></h2>
@@ -15,6 +15,23 @@
 </div>
 
 <style>
+    .books-grid {
+        display: grid; 
+        gap: 1rem;
+    }
+
+    @media screen and (min-width: 480px) {
+        .books-grid {
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
+
+    @media screen and (min-width: 900px) {
+        .books-grid {
+            grid-template-columns: repeat(3, 1fr);
+        }
+    }
+
     img {
         width: 240px;
         max-width: 240px;
