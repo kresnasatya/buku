fa86588569b32433d92df659b907f412c5f66f4a
Kresna Satya <kresnasatya72@gmail.com>
2024-12-18 21:57:00 +0800

    refactor: replace front-matter with gray-matter

--- book.js
+++ book.js
@@ -1,16 +1,16 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
 import markdownit from 'markdown-it';
-import fm from 'front-matter';
+import matter from 'gray-matter';
 
 (async() => {
     const files = await fs.readdir(path.join(process.cwd(), 'static/books'));
     const mdFiles = files.filter(file => path.extname(file).toLowerCase() === '.md');
 
-    const data = [];
+    const output = [];
     for (const mdFile of mdFiles) {
-        const content = await fs.readFile(path.join(process.cwd(), 'static/books', mdFile), 'utf8');
-        const { attributes, body } = fm(content);
+        const rawContent = await fs.readFile(path.join(process.cwd(), 'static/books', mdFile), 'utf8');
+        const { content: body, data: attributes } = matter(rawContent);
         const { title, image, author } = attributes;
         const slug = mdFile.split('.md')[0];
         
@@ -19,8 +19,8 @@ import fm from 'front-matter';
         });
         const reviewHtml = md.render(body);
 
-        data.push({ title, slug, image, author, reviewHtml });
+        output.push({ title, slug, image, author, reviewHtml });
     }
 
-    fs.writeFile(path.join(process.cwd(), 'src/lib/books.json'), JSON.stringify(data, null, " "), null);
+    fs.writeFile(path.join(process.cwd(), 'src/lib/books.json'), JSON.stringify(output, null, " "), null);
 })();
